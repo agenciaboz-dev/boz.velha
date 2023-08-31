e4772a2466dc528b2b2910a4d9ad65ac6abb38aa
ispadilha <israel.padilha@gmail.com>
2023-08-31 11:58:36 -0300

    Starting logic

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+  const [board, setBoard] = useState([
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', ''],
+  ]);
+
+  const handleSlotPress = (row, col) => {
+    // Check if the slot is already filled or if the game is over
+    // if (board[row][col] === '' && !gameOver) {
+    //   const newBoard = [...board];
+    //   newBoard[row][col] = currentPlayer === 'X' ? 'X' : 'O';
+    //   setBoard(newBoard);
+    //   setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X');
+    // }
+
+    const newBoard = [...board];
+    newBoard[row][col] = 'X' ? 'O' : 'X';
+    setBoard(newBoard);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{
+      flex: 1,
+      flexDirection: 'row',
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}>
+      <View>
+        {board.map((row, rowIndex) => (
+          <View key={rowIndex} style={{ flexDirection: 'row' }}>
+            {row.map((slot, colIndex) => (
+              <TouchableOpacity
+                key={colIndex}
+                style={{
+                  borderWidth: 5,
+                  height: 100,
+                  width: 100,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+                onPress={() => handleSlotPress(rowIndex, colIndex)}
+              >
+                <Text style={{ fontSize: 36 }}>{slot}</Text>
+              </TouchableOpacity>
+            ))}
+          </View>
+        ))}
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
