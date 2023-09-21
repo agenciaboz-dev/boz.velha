2a68169fbf6221f4cf66b74d7fc2681877d977cd
ispadilha <israel.padilha@gmail.com>
2023-09-21 11:23:28 -0300

    Created theme.js

--- App.js
+++ App.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import AppRoutes from './src/routes';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+    let [loaded] = useFonts({
+        KGPrimaryPenmanshipAlt: require("./assets/fonts/kg_primary_penmanship/KGPrimaryPenmanshipAlt.ttf"),
+    })
+
+    if (!loaded) {
+        return <></>
+    }
+
     return (
         <AppRoutes />
     )
--- src/hooks/useColors.js
+++ src/hooks/useColors.js
@@ -1,4 +1,4 @@
-import { colors } from '../utils/colors';
+import { colors } from '../style/colors';
 
 export function useColors() {
   return colors;
