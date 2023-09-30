c7e764db55ee16c7d78673b5a6d47f850784c83a
ispadilha <israel.padilha@gmail.com>
2023-09-30 14:32:58 -0300

    Updated Slots to render image not text

--- src/components/Slot/index.js
+++ src/components/Slot/index.js
@@ -1,37 +1,34 @@
 import React from 'react';
-import { TouchableOpacity, Text, Image } from 'react-native';
+import { TouchableOpacity, Image } from 'react-native';
 import { useColors } from '../../hooks/useColors';
+import images from '../../utils/images';
 
 export default function Slot({ rowIndex, colIndex, updateBoard, slot, player1Symbol }) {
   const colors = useColors();
 
+  let slotImage;
+  if (slot === 'X') {
+    slotImage = images.x;
+  } else if (slot === 'O') {
+    slotImage = images.circle;
+  } else {
+    slotImage = null;
+  }
+
   return (
     <TouchableOpacity
-      style={[
-          {
-          backgroundColor: colors.primary,
-          margin: 5,
-          height: 50,
-          width: 50,
-          justifyContent: 'center',
-          alignItems: 'center',
-          borderRadius: 5
-          },
-      ]}
+      style={{
+        backgroundColor: colors.primary,
+        margin: 5,
+        height: 50,
+        width: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 5
+      }}
       onPress={() => updateBoard(rowIndex, colIndex)}
     >
-      {/* <Image source={player1Symbol == 'O' ? images.circle : images.x }
-        style={{
-          height: 30,
-          width: 30,
-          resizeMode: "contain",
-      }}
-      /> */}
-      <Text style={{
-          fontSize: 18,
-          }}>
-          {slot}
-      </Text>
+      {slotImage && <Image source={slotImage} style={{ height: 30, width: 30, resizeMode: 'contain' }} />}
     </TouchableOpacity>
   );
 }
