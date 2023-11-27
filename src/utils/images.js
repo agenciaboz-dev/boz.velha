aa0f12400328ba84577938c73502bec64ce689de
ispadilha <israel.padilha@gmail.com>
2023-11-27 11:50:22 -0300

    update: images

--- src/utils/images.js
+++ src/utils/images.js
@@ -1,8 +1,7 @@
 const images = {
-
     background: require("../../assets/images/181.jpg"),
     boz_logo: require("../../assets/images/logo_boz.png"),
-    game_logo: require("../../assets/images/logo_game.png"),
+    game_logo: require("../../assets/images/logo_game_new.png"),
     close: require("../../assets/images/Vector.png"),
     bozzer_jumping: require("../../assets/images/Bozzer_jumping.png"),
     bozzer_writing: require("../../assets/images/Bozzer_writing.png"),
@@ -11,7 +10,6 @@ const images = {
     check: require("../../assets/images/ok.png"),
     circle: require("../../assets/images/CIRCLE.png"),
     x: require("../../assets/images/X.png"),
-
 }
 
 export default images
